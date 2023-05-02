b9da0a60e8c74376e76ba77bf16b95451bcc028f
sobsdavlatov <sobsdavlatov27@gmail.com>
2023-05-03 00:28:55 +0500

    preprocessing data

--- utilities.py
+++ utilities.py
@@ -14,36 +14,35 @@ def preprocess_json(json_path, name):
     for filename in os.listdir(json_path):
         if filename.endswith(".json"):
             json_file = os.path.join(json_path, filename)
-    with open(json_file, "r") as f:
+    with open(json_file, 'r') as f:
         data = json.load(f)
-    
-    filenames = []
-    bouninding_boxes = []
-    for image in data["images"]:
-        filenames.append(image["file_name"])
-    for annotation in data["annotations"]:
-        #Remove brackets from bounding boxes
-        bbox = str(annotation["bbox"]).strip("[]")
-        #Split bounding boxes into separate values
-        bbox_values = bbox.split(",")
-        #Appeding each value to the corresponding list
+
+    file_names = []
+    bounding_boxes = []
+    for image in data['images']:
+        file_names.append(image['file_name'])
+    for annotation in data['annotations']:
+        # Remove brackets from bounding boxes
+        bbox = str(annotation['bbox']).strip('[]')
+        # Split bounding box into separate values
+        bbox_values = bbox.split(',')
+        # Append each value to the list of bounding boxes
         for value in bbox_values:
-            bbox_values.append(value)
-    #Check whether the directory exist, if non-existant, create
-    exist = os.path.exists("dataset/preprocessed/")
+            bounding_boxes.append(value)
+    file_path = 'dataset/preprocessed/{}.csv'.format(name)
+    # Write the filenames and bounding boxes to a CSV file
+    exist = os.path.exists('dataset/preprocessed')
     if not exist:
-        os.makedirs("dataset/preprocessed/")
-    file_path = "dataset/preprocessed/{}.csv".format(name)
-    #Write the filenames and bouding boxes to a CSV file
+        os.makedirs('dataset/preprocessed')
     with open(file_path, 'w', newline='') as f:
         writer = csv.writer(f)
-        #Iterate over the data to write into a csv file
-        for i in range(len(filenames)):
-            #Split bouding box values into separate variables
-            x, y, w, h = bouninding_boxes[i*4:(i+1)*4]
-            #Write vaalues to a row in the CSV file
-            writer.writerow([filenames[i], x, y,w,h])
-    
+        # Iterate over the data and write to file
+        for i in range(len(file_names)):
+            # Split bounding box values into separate variables
+            x, y, w, h = bounding_boxes[i*4:(i+1)*4]
+            # Write values to a row in the CSV file
+            writer.writerow([file_names[i], x, y, w, h])
+
     return file_path
 
 #Read csv and load data
