6b6e6df489eef534e06df1124bbacb15053d4302
sobsdavlatov <sobsdavlatov27@gmail.com>
2023-05-02 23:20:46 +0500

    added new function to load csv to X and y, and preprocessed data

---
+++ preprocess.py
@@ -0,0 +1,20 @@
+import os 
+import tensorflow as tf 
+import utilities as ut
+
+#Define path to dataset 
+base_dir = "dataset/"
+train_dir = os.path.join(base_dir, "train")
+valid_dir = os.path.join(base_dir, "valid")
+test_dir = os.path.join(base_dir, "test")
+
+#Preprocess json file into csv to simplify data loading
+train_csv = ut.preprocess_json(train_dir, "train")
+valid_csv = ut.preprocess_json(valid_dir, "valid")
+test_csv = ut.preprocess_json(test_dir, "test")
+
+
+#Load dataset into X and y
+X_train, y_train = ut.read_csv_and_load(train_csv, train_dir)
+X_valid, y_valid = ut.read_csv_and_load(valid_csv, valid_dir)
+X_test, y_test = ut.read_csv_and_load(test_csv, test_dir)
--- utilities.py
+++ utilities.py
@@ -1,8 +1,11 @@
 import os 
+import cv2
 import csv
 import json
 import numpy as np 
 import PIL as Image
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import img_to_array
 
 #preprocess json into csv to simplify data loading
 
@@ -42,3 +45,39 @@ def preprocess_json(json_path, name):
             writer.writerow([filenames[i], x, y,w,h])
     
     return file_path
+
+#Read csv and load data
+def read_csv_and_load(csv_path, image_path):
+    #Read csv file
+    rows = open(csv_path).read().strip().split('\n')
+    data = []
+    targets = []
+    filenames = []
+    for row in rows:
+        row = row.split(",")
+        #Filename and bounding box 
+        (filename, startX, startY, endX, endY) = row
+        imagepaths = os.path.join(image_path, filename)
+        image = cv2.imread(imagepaths)
+        (h,w) = image.shape[:2]
+        #Initialize starting point
+        startX = float(startX) / w
+        startY = float(startY) / h
+        #Initialize ending point 
+        endX = float(endX) / w
+        endY = float(endY) / h
+        #Lod iamge and define size 
+        image = load_img(imagepaths, target_size = (224,224))
+        image = img_to_array(image)
+        #Appending to corresponing lists
+        targets.append((startX, startY, endX, endY))
+        filenames.append(filename)
+        data.append(image)
+
+    #Normalize data
+    data = np.array(data, dtype="float32") / 255.0
+    targets = np.array(targets, dtype="float32") / 255.0
+    X = data
+    y = targets 
+
+    return X, y
