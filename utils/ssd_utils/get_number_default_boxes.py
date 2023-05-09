398cb51af86545d51074c4215964f868e7abf376
Sobirjon Davlatov <132314169+sobsdavlatov@users.noreply.github.com>
2023-05-09 12:28:25 +0500

    Delete utils directory

--- utils/ssd_utils/get_number_default_boxes.py
+++
@@ -1,3 +0,0 @@
-def get_number_default_boxes(aspect_ratios, extra_box_for_ar_1 = True):
-    num_aspect_ration = len(aspect_ratios)
-    return num_aspect_ration + 1 if (1.0 in aspect_ratios) and extra_box_for_ar_1 else num_aspect_ration
